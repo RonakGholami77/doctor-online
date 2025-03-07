fb1c484fc89f862d600fea3350c0a3cb470ed32a
Rgh <gholamironak88@yahoo.com>
2025-03-07 06:09:13 +0330

    [Add] prettier and config it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {Vazirmatn , Markazi_Text , Noto_Kufi_Arabic } from "next/font/google";
+import { Vazirmatn, Markazi_Text, Noto_Kufi_Arabic } from "next/font/google";
 
 const vazirmatn = Vazirmatn({
-  subsets: ['latin' , 'arabic'],
-  variable: '--font-vazirmatn',
-  display: 'swap',
-})
+  subsets: ["latin", "arabic"],
+  variable: "--font-vazirmatn",
+  display: "swap",
+});
 
 const markazi = Markazi_Text({
-  subsets: ['latin' , 'arabic'],
-  variable: '--font-markazi',
-  display: 'swap',
-})
+  subsets: ["latin", "arabic"],
+  variable: "--font-markazi",
+  display: "swap",
+});
 
-const kufi =  Noto_Kufi_Arabic({
-  subsets: ['latin' , 'arabic'],
-  variable: '--font-noto-Kufi-Arabic',
-  display: 'swap',
-})
+const kufi = Noto_Kufi_Arabic({
+  subsets: ["latin", "arabic"],
+  variable: "--font-noto-Kufi-Arabic",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "دکتر آنلاین",
@@ -31,10 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa" dir="rtl" className={`${vazirmatn.variable} ${markazi.variable} ${kufi.variable}`}>
-      <body >
-        {children}
-      </body>
+    <html
+      lang="fa"
+      dir="rtl"
+      className={`${vazirmatn.variable} ${markazi.variable} ${kufi.variable}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
