390f76705c5dc940b58e7b5fe66cba379ae12f3d
Rgh <gholamironak88@yahoo.com>
2025-03-07 06:12:16 +0330

    [Add] directories

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function Page(): ReactElement {
+  return (
+    <div>
+      <h1>درباره ی ما !!!</h1>
+    </div>
+  );
+}
---
+++ src/app/users/page.tsx
@@ -0,0 +1,20 @@
+type User = {
+  id: number;
+  name: string;
+};
+
+export default async function Page() {
+  const usersFetch = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await usersFetch.json();
+
+  return (
+    <div>
+      <h1>کاربران</h1>
+      <ol>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
